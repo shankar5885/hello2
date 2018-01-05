fff803348713e50980601d2ddaf0fc3e71020eb2
shankar5885 <rao.shankararao@gmail.com>
2018-01-05 16:18:15 +0530

    first commit

---
+++ stars.java
@@ -0,0 +1,10 @@
+class stars {
+  public static void main(String[] args) {
+for (int row = 1; row <= 10; row++) {
+for(int numberOfStars = 1; numberOfStars <= row; numberOfStars++) {
+System.out.print("*");
+      }
+System.out.println(); 
+    }
+  }
+}
